588d156bf2b6fe0bd430b61abe99c918506ae41c
twells16 <tannerwells03@gmail.com>
2025-11-03 15:51:51 -0600

    Refactor comments and enhance clarity in random walk code
    Updated comments and improved code clarity for GPU random walk implementation.

--- P_GPURandomWalk.cu
+++ P_GPURandomWalk.cu
@@ -57,7 +57,12 @@ int getRandomDirection();
 int main(int, char**);
 
 /*GPU Kernel that will run our drunk walk. 
-This passes in our variables for our X, and Y positions as well as uses our seed
+This defines our variables for our X, and Y positions as well as 
+our seed that is whole positive numbers, This then makes our unique thread id,
+we then define the cudaranState that defines our random numbers in state,
+we then set a random generator and sets it parameters,
+we then generate a randome float between 0.0-1.0,
+we then use an if/else statement to check where the randome float is
 */
 __global__ void randomWalk(int *positionsX, int *positionsY, unsigned long long seed)
 {
@@ -75,8 +80,15 @@ __global__ void randomWalk(int *positionsX, int *positionsY, unsigned long long 
 		float randX = curand_uniform(&state);
 		float randY = curand_uniform(&state);
 
-		posX += (randX < 0.5f)? -1 : 1;
-		posY += (randY < 0.5f)? -1 : 1; 
+		if (randX < 0.5f)
+    	posX += -1;
+		else
+  		posX += 1;
+
+		if (randY < 0.5f)
+		posY += -1;
+		else
+		posY +=1;
 	}
 
 	positionsX[tid] = posX;
@@ -84,7 +96,8 @@ __global__ void randomWalk(int *positionsX, int *positionsY, unsigned long long 
 
 }
 
-
+/* This grabs what direction our person will move
+using the rand function*/
 int getRandomDirection()
 {	
 	int randomNumber = rand();
@@ -97,22 +110,24 @@ int main(int argc, char** argv)
 {
 	int *positionsX, *positionsY;
 
-	//
+	//allows automatic transfer of data between the host and device
+	//and allows for unified data access from the CPU and GPU
 	cudaMallocManaged(&positionsX, NUM_WALKS * sizeof(int));
 	cudaMallocManaged(&positionsY, NUM_WALKS * sizeof(int));
-	//
+	//This sets the number of threads per block and finds how many blocks you will use
 	int threadsPerBlock = 20;
 	int blocks = (NUM_WALKS + threadsPerBlock - 1) / threadsPerBlock;
+	//this uses time of day to make your seed
 	unsigned long long seed = time(NULL);
-	//
+	//this calls the kernel you made above
 	randomWalk<<<blocks, threadsPerBlock>>>(positionsX, positionsY, seed);
-	//
+	//This syncs the host and the device 
 	cudaDeviceSynchronize();
-	/**/
+	/*Removed the CPU code*/
 	//srand(time(NULL));
 	
 	printf("RAND_MAX for this implementation is = %d \n", RAND_MAX);
-	/**/
+	/*Removed the CPU function*/
 	/*
 	int positionX = 0;
 	int positionY = 0;
@@ -124,7 +139,7 @@ int main(int argc, char** argv)
 	
 	printf("\n Final position = (%d,%d) \n", positionX, positionY);
 	*/
-	/**/
+	/*This prints each walk and its position as well the final position of our drunk person*/
 	printf("\nFinal Positions for %d walks : \n", NUM_WALKS);
 	for (int i = 0; i < NUM_WALKS; i++)
 	{
@@ -132,9 +147,10 @@ int main(int argc, char** argv)
 	}
 
 
-	//
+	//This frees our memory from the GPU that we stored.
 	cudaFree(positionsX);
 	cudaFree(positionsY);
 	return 0;
 }
+
 
